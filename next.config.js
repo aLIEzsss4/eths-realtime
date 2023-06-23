64ededef3640ed8b5ab4afa21c5c6660bcfcc59c
aLIEzsss4 <vuetomato@gmail.com>
2023-06-23 17:49:29 +0800

    ignoreBuildErrors typescript

--- next.config.js
+++ next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
