55e133ad9d2dc2fdbfdd2a67cf9d682eea9e1973
aLIEzsss4 <vuetomato@gmail.com>
2023-06-23 19:51:25 +0800

    //@ts-nocheck

--- src/app/api/realtime/route.ts
+++ src/app/api/realtime/route.ts
@@ -1,3 +1,4 @@
+//@ts-nocheck
 // import supabase from '../../supabase';
 import EventSource from "eventsource";
 import { NextApiRequest, NextApiResponse } from 'next';
@@ -8,15 +9,16 @@ export const dynamic = 'force-dynamic';
 export const runtime = "edge"
 
 
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
-
 console.log(supabase.supabaseUrl)
 
 
-export function GET(req: NextApiRequest, res: NextApiResponse) {
+
+export function  GET(req: NextApiRequest, res: NextApiResponse) {
   const table = 'ethscriptions'
   // const { action } = req.query;
   // console.log({q:req,url:req.url})
@@ -33,6 +35,8 @@ export function GET(req: NextApiRequest, res: NextApiResponse) {
 
   
   // let arr=[]
+  
+
   const { readable, writable } = new TransformStream();
   const writer = writable.getWriter();
   const encoder = new TextEncoder();
