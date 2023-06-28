095f9058505201ab7d13a27d29337ebf3bbed77d
aLIEzsss4 <vuetomato@gmail.com>
2023-06-29 01:36:45 +0800

    remove api

--- src/app/compoents/RealtimeUpdates.tsx
+++ src/app/compoents/RealtimeUpdates.tsx
@@ -11,25 +11,36 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
+interface RenderTextProps {
+  text: string
+}
+
+const RenderText = ({ text }: RenderTextProps) => {
+  if (text.startsWith('data:image/')) {
+    return <img src={text} />
+  } else {
+    return <p>{text}</p>
+  }
+}
 
 function RealtimeUpdates() {
   const [messages, setMessages] = useState([]);
 
 
-  const relatimeFn=()=>{
+  const relatimeFn = () => {
     const subscription = supabase.channel('custom-all-channel')
-    .on(
-      'postgres_changes',
-      { event: '*', schema: 'public', table: 'ethscriptions' },
-      async (payload) => {
-        console.log('Change received!', payload)
-        const newData = JSON.stringify(payload.new)
-      setMessages((prevMessages) => [...prevMessages, JSON.parse(newData)]);
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'ethscriptions' },
+        async (payload) => {
+          console.log('Change received!', payload)
+          const newData = JSON.stringify(payload.new)
+          setMessages((prevMessages) => [...prevMessages, JSON.parse(newData)]);
 
-        // await writer.write(encoder.encode("data: " + newData + "\n\n"));
-      }
-    )
-    .subscribe()
+          // await writer.write(encoder.encode("data: " + newData + "\n\n"));
+        }
+      )
+      .subscribe()
   }
 
 
@@ -49,19 +60,17 @@ function RealtimeUpdates() {
     relatimeFn()
   }, []);
 
-  const exit=()=>{
-    fetch('/api/realtime?action=exit')
-      .then(res=>console.log(res))
-      .catch(console.log)
-  }
+  
 
   return (
     <div>
-      <div onClick={exit}>x</div>
       {
         [...messages].reverse().map((message, index) => (
           <div key={index} >
-            <div className='mb-4 w-full font-mono border aspect-square p-4 grid place-items-center whitespace-pre-wrap break-all overflow-x-hidden overflow-y-auto'>{message.decode_data} </div>
+            <div className='mb-4 w-full font-mono border aspect-square p-4 grid place-items-center whitespace-pre-wrap break-all overflow-x-hidden overflow-y-auto'>
+              {/* <p dangerouslySetInnerHTML={{ __html: message.decode_data }}></p> */}
+              <RenderText text={message!.decode_data as string} />
+            </div>
             <p>Owner:{message.owner} </p>
             <p>Creator:{message.creator} </p>
             <p>Created:{dayjs(message.block_time).fromNow()} </p>
